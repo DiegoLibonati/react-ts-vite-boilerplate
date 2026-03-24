1814624b626737d196139e2c85fddad5e93a1627
Die <dlibonati@leafnoise.io>
2026-03-24 12:47:57 -0300

    fix: styles of description in not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,9 +4,7 @@ const NotFoundPage = () => {
   return (
     <main className="not-found-page">
       <h1 className="title">Page Not Found</h1>
-      <p className="not-found-page__description">
-        The page you're looking for doesn't exist or has been moved.
-      </p>
+      <p className="description">The page you're looking for doesn't exist or has been moved.</p>
     </main>
   );
 };
